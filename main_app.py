4065118de4d5cf4b3d6d56fd07d2179bfbd2fa91
Thomasgeorge04 <16093@my.sanctamaria.school.nz>
2022-02-09 10:56:10 +1300

    Created about page

--- main_app.py
+++ main_app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/") #route to main page
 def hello_world():
-    return "<h1>Welcome to my site</h1>"
+    return "<h1>Home Page</h1>"
+
+@app.route("/about") #route to main page
+def about():
+    return "<h1>About Page</h1>"
+
 
 if __name__=='__main__':
     app.run(debug=True)
