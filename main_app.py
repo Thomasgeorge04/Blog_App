a7f05e2ca552c8fdb03d1b569b43730b6b91393b
Thomasgeorge04 <16093@my.sanctamaria.school.nz>
2022-02-28 20:40:41 +1300

    Routes
    added routes to login and register pages

--- main_app.py
+++ main_app.py
@@ -1,8 +1,9 @@
 #This is a blog app that allows user to register and post blogs
 
 from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from forms import RegistrationForm, LoginForm
 
+app = Flask(__name__)
 app.config['SECRET KEY'] = '2422241e90658058902011e403344b45'
 
 #List of posts
@@ -32,15 +33,29 @@ posts = [
     
 ]
 
+#Route for home page
 @app.route("/") #route to main page
 @app.route("/home") #route to home page
 def home():
     return render_template('home.html', posts=posts)
 
+#Route for about page
 @app.route("/about") #route to about page
 def about():
     return render_template('about.html', title='About')
 
+#Route for registration page
+@app.route("/register") #route to about page
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
+#Route for login page
+@app.route("/login") #route to about page
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+#Allows main app to run the web server
 if __name__=='__main__':
     app.run(debug=True)
